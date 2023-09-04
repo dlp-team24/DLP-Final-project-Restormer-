3b7d380cd7f0ca2627659980b62717192ac73c85
Yvonne-ul6u45p <139047113+Yvonne-ul6u45p@users.noreply.github.com>
2023-09-04 21:23:20 +0800

    Update image.py
    Upper case to Lower case

--- compressai/zoo/image.py
+++ compressai/zoo/image.py
@@ -70,10 +70,10 @@ model_architectures = {
     "cheng2020-attn": Cheng2020Attention,
     "tic_hp": TIC_hp,
     'tic_promptmodel': TIC_PromptModel,
-    "TICwRSB_v1": TICwRSB_v1,
-    "TICwRSB_v2": TICwRSB_v2,
-    "TICwRSBwMLP_v1":TICwRSBwMLP_v1,
-    "TICwRSBwMLP_v2":TICwRSBwMLP_v2,
+    "ticwrsb_v1": TICwRSB_v1,
+    "ticwrsb_v2": TICwRSB_v2,
+    "ticwrsbwmlp_v1":TICwRSBwMLP_v1,
+    "ticwrsbwmlp_v2":TICwRSBwMLP_v2,
 }
 
 root_url = "https://compressai.s3.amazonaws.com/models/v1"
